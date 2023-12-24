a66b6fa8b706ef044f0c6a138e1bc1da5808787e
DhanishMA <danishma2099pi@gmail.com>
2023-12-24 23:00:33 +0530

    0

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 CssClass = "SlideOut";
 
 function ShowNowPlaying() {
-  var div = document.getElementById("NowPlayingDiv");
+  var div = document.getElementById("NowPlayingList");
   div.style.display = "block"
   if (CssClass === "SlideOut") {
     div.classList.remove("SlideOut");
@@ -15,3 +15,20 @@ function ShowNowPlaying() {
   }
 
 }
+
+function ShowSideBar() {
+  var div = document.getElementById("SideBarId");
+  div.style.display = "block"
+  if (CssClass === "SlideOut") {
+    div.classList.remove("SlideOut");
+    div.classList.toggle("SlideIn");
+    CssClass = "SlideIn";
+  } 
+  else {
+    div.classList.remove("SlideIn");
+    div.classList.toggle("SlideOut");
+    CssClass = "SlideOut";
+  }
+
+}
+
